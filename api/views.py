21d401b8b531143192ac7e90bc1d1fcd1cb52cc5
rdogm13 <drg6525@yahoo.com>
2021-04-03 18:36:05 -0700

    serializer added / basic task list endpoint

--- api/views.py
+++ api/views.py
@@ -4,8 +4,11 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# from snippets.models import Snippet
-# from snippets.seralizers import SnippetSerializer
+
+from .serializers import TaskSerializer
+
+from .models import Task
+
 
 # Create your views here.
 
@@ -19,3 +22,9 @@ def apiOverview(request):
         'Delete':'/task-delete/<str:pk>/'
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
