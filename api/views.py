fa798d5f5936666f5e1b942f231f80854a028f1e
rdogm13 <drg6525@yahoo.com>
2021-04-03 18:26:44 -0700

    added rest framework -- overview endpoint with API routes added

--- api/views.py
+++ api/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from snippets.models import Snippet
+# from snippets.seralizers import SnippetSerializer
+
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls = {
+        'List':'/task_list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/'
+    }
+    return Response(api_urls)
